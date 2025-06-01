494c075f242b10cf31c98d2a98a4df31b3d65bb4
ChrisAchkar <caa152@pitt.edu>
2025-06-01 15:40:17 -0400

    added comments to evaluator.py

--- backend/evaluator.py
+++ backend/evaluator.py
@@ -3,31 +3,40 @@ import pronouncing
 import difflib
 import librosa
 
+# initialize automatic speech recognition (ASR) pipeline
 asr = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
 
+# loads audio file
 def load_audio(path):
-    audio, _ = librosa.load(path, sr=16000)
+    audio, _ = librosa.load(path, sr=16000) # change sample rate per model
     return audio
 
+# uses ASR pipeline to transcribe audio
 def transcribe(audio):
     result = asr(audio)
     return result['text'].lower().strip()
 
+# looks up phoneme spelling of a word
 def words_to_phonemes(word):
     phones = pronouncing.phones_for_word(word)
     return phones[0] if phones else ""
 
+# calculates similiarty ratio between the target and transcribed phonemes
 def phoneme_similarity(target_phonemes, transcribed_phonemes):
     similarity = difflib.SequenceMatcher(None, target_phonemes.split(), transcribed_phonemes.split())
     return similarity.ratio()
 
+# evaluates pronouncation
 def evaluate(audio_path, target_word):
+    # load audio and run speech recognition
     audio = load_audio(audio_path)
     transcription = transcribe(audio)
 
+    # convert both to phonemes
     target_phonemes = words_to_phonemes(target_word.lower())
     transcribed_phonemes = words_to_phonemes(transcription)
 
+    # use phoneme similary if possible, otherwise text comparison
     if not target_phonemes or not transcribed_phonemes:
         score = difflib.SequenceMatcher(None, target_word.lower(), transcription).ratio()
     else:
@@ -38,9 +47,9 @@ def evaluate(audio_path, target_word):
         "score": round(score, 3)
     }
 
-if __name__ == "__main__":
-    file_audio = "/Users/sumankrishna/Desktop/RepeatAfterMe.ai/banayna.m4a"
-    target_word = "banana"
-    result = evaluate(file_audio, target_word)
-    print("Transcribed:", result["transcription"])
-    print("Pronunciation score:", result["score"])
+# main
+file_audio = "/Users/Christopher/Desktop/banana.m4a"
+target_word = "banana"
+result = evaluate(file_audio, target_word)
+print("Transcribed:", result["transcription"])
+print("Pronunciation score:", result["score"])
