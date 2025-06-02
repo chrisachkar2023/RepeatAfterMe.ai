cd04a7aa74c8b3f937811d3be0a49054db2bee02
ChrisAchkar <caa152@pitt.edu>
2025-06-02 19:16:18 -0400

    updated html page

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, Request, UploadFile, Form
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import random
 from evaluator import evaluate
 
 app = FastAPI()
-templates = Jinja2Templates(directory="../frontend")
+templates = Jinja2Templates(directory="frontend")
 
 # root (home page)
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    words = ["banana", "apple", "orange", "grape", "kiwi"]
+    words = ["Banana", "Apple", "Orange", "Grape", "Kiwi"]
     word = random.choice(words)
     return templates.TemplateResponse("index.html", {"request": request, "word": word})
