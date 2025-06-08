8e8b230f95b802131a1354a39071a8e02c7d3efa
ChrisAchkar <caa152@pitt.edu>
2025-06-08 16:09:12 -0400

    used sqlite3 to add word difficulty

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="frontend")
 
 def get_easy_words():
-    connect = sqlite3.connect("words.db")
+    connect = sqlite3.connect("backend/words.db")
     cursor = connect.cursor()
     cursor.execute("SELECT word FROM words WHERE difficulty = 'easy'")
     words = [row[0] for row in cursor.fetchall()]
@@ -18,7 +18,7 @@ def get_easy_words():
     return words
 
 def get_medium_words():
-    connect = sqlite3.connect("words.db")
+    connect = sqlite3.connect("backend/words.db")
     cursor = connect.cursor()
     cursor.execute("SELECT word FROM words WHERE difficulty = 'medium'")
     words = [row[0] for row in cursor.fetchall()]
@@ -26,7 +26,7 @@ def get_medium_words():
     return words
 
 def get_hard_words():
-    connect = sqlite3.connect("words.db")
+    connect = sqlite3.connect("backend/words.db")
     cursor = connect.cursor()
     cursor.execute("SELECT word FROM words WHERE difficulty = 'hard'")
     words = [row[0] for row in cursor.fetchall()]
@@ -36,7 +36,7 @@ def get_hard_words():
 # root (home page)
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    words = get_easy_words()
+    words = get_hard_words()
     word = random.choice(words)
     return templates.TemplateResponse("index.html", {"request": request, "word": word})
 
