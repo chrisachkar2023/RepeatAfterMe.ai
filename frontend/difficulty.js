16d11ebb0d4353583b5bc631773da9dcc09db070
ChrisAchkar <caa152@pitt.edu>
2025-06-10 20:40:23 -0400

    added button to change given word within the same difficulty

--- frontend/difficulty.js
+++ frontend/difficulty.js
@@ -1,3 +1,4 @@
+// select difficulty
 document.getElementById("difficulty").addEventListener("change", function () {
     const selectedDifficulty = this.value;
 
@@ -6,6 +7,22 @@ document.getElementById("difficulty").addEventListener("change", function () {
         .then(word => {
             document.getElementById("word").textContent = word;      // update displayed word
             document.getElementById("hidden-word").value = word;     // update hidden input value
+        })
+        .catch(error => {
+            console.error("Error fetching word:", error);
+        });
+});
+
+
+// change word button
+document.getElementById("change-word-btn").addEventListener("click", () => {
+    const selectedDifficulty = document.getElementById("difficulty").value;
+
+    fetch(`/api/word?difficulty=${selectedDifficulty}`)
+        .then(response => response.text())
+        .then(word => {
+            document.getElementById("word").textContent = word;
+            document.getElementById("hidden-word").value = word;
         })
         .catch(error => {
             console.error("Error fetching word:", error);
