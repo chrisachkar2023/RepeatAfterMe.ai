b4976ab7aaf5d31feec2a9692da2ef99d2d6014e
chrisachkar2023 <cachkar0408@gmail.com>
2025-06-28 19:15:25 +0000

    removed testing file

--- backend/text-to-speech.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-text = "Hello! This is a test using gTTS with an Indian English accent."
-
-tts = gTTS(text=text, lang='en', tld='co.in', slow=False)
-tts.save("output_audio.mp3")
-
-print("Audio saved successfully.")
