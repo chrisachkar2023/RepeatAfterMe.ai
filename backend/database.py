f96d718d20eb43522b08d0b912d984c30041414f
chrisachkar2023 <cachkar0408@gmail.com>
2025-07-16 20:50:29 -0400

    deleted old sqllite db files and created saved_words table in postgres database

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 import os
 
-# set up database
+# set up postgres database
 DATABASE_URL = os.getenv("DATABASE_URL") # environmental var
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -21,6 +21,18 @@ class Word(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     word = Column(Text, nullable=False)
     difficulty = Column(Text, nullable=False)
+    
+# saved words 
+class SavedWord(Base):
+    __tablename__ = "saved_words"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    word_id = Column(Integer, ForeignKey("words.id"), nullable=False)
+
+    user = relationship("User", backref="saved_words")
+    word = relationship("Word")
+
+    __table_args__ = (UniqueConstraint("user_id", "word_id", name="_user_word_uc"),)
 
 # create tables if they don't exist
 Base.metadata.create_all(bind=engine)
